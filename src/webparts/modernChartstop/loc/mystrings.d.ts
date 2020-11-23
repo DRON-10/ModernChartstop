55ce4f4394009d740815059b3b10bf40fc707321
DRON-10 <nilanjan1012@gmail.com>
2020-11-23 14:30:02 +0530

    Initial commit

---
+++ src/webparts/modernChartstop/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IModernChartstopWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'ModernChartstopWebPartStrings' {
+  const strings: IModernChartstopWebPartStrings;
+  export = strings;
+}
